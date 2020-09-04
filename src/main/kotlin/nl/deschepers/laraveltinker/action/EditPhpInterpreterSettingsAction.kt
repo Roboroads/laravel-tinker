369fc25bf84a1ab24c12f8e4b09d055fb3d171c4
Robbin Schepers <robbin@deschepers.nl>
2020-09-04 20:23:51 +0200

    Fixes #13 and resolves #10 Fixes link to PHP interpreter settings Last command will be caches for when you reopen the console

--- src/main/kotlin/nl/deschepers/laraveltinker/action/EditPhpInterpreterSettingsAction.kt
+++ src/main/kotlin/nl/deschepers/laraveltinker/action/EditPhpInterpreterSettingsAction.kt
@@ -4,12 +4,19 @@ import com.intellij.notification.Notification
 import com.intellij.notification.NotificationAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.options.ShowSettingsUtil
+import com.jetbrains.php.config.PhpProjectConfigurable
+import nl.deschepers.laraveltinker.LaravelTinkerBundle
 
-class EditPhpInterpreterSettingsAction : NotificationAction("PHP Interpreter Settings") {
+class EditPhpInterpreterSettingsAction : NotificationAction(
+    LaravelTinkerBundle.message("lt.php.interpreter.settings")
+) {
     override fun actionPerformed(e: AnActionEvent, notification: Notification) {
         val project = e.project
         if (project != null) {
-            ShowSettingsUtil.getInstance().showSettingsDialog(project, "Interpreter")
+            ShowSettingsUtil.getInstance().showSettingsDialog(
+                project,
+                PhpProjectConfigurable::class.java
+            )
         }
     }
 }
